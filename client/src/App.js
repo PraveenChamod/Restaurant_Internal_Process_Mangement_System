2e6c639e7b06a341717814287d7ab118326343a6
Dilmith98 <dilmithdilhara.gg@gmail.com>
2023-02-17 10:51:28 +0530

    created Review controler and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
-import {BrowserRouter as Router, Outlet, Route, Routes} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 import {Home} from './Pages/Home';
 import {Menu} from './Pages/Menu'
 import {Cart} from './Pages/Customer/Cart'
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import LoginPg from './Pages/Login';
-import { Fragment, useEffect } from 'react';
+
+import { useEffect } from 'react';
 import ScrollToTop from './Hooks/ScrollToTop';
 import AdminDashBoard from './Pages/Admin/AdminDashBoard';
+import AddUser from './Pages/Admin/AddUser';
+import ViewUser from './Pages/Admin/ViewUser';
 import AdminUserProfile from './Pages/Admin/UserProfile';
 import ManagerDashBoard from './Pages/Manager/ManagerDashBoard';
 import AddOutletStaff from './Pages/Manager/AddUser';
+import ViewStaffMember from './Pages/Manager/ViewUser';
 import ManagerUserProfile from './Pages/Manager/UserProfile';
 import ReportGeneration from './Pages/Manager/ReportGeneration';
 import ReportPreview from './Pages/Manager/ReportPreview';
@@ -20,7 +24,8 @@ import AddStockItem from './Pages/Manager/AddStockItem';
 import ViewStock from './Pages/Manager/ViewStock';
 import ViewItem from './Pages/Manager/ViewItem';
 import StaffMemberUserProfile from './Pages/Staff-Member/UserProfile';
-import AddOffers from './Pages/Manager/AddOffers';
+import AddOffers from './Pages/SharedOnes/AddOffers';
+import OrderDetails from './Pages/Staff-Member/OrderDetails';
 import TableReservationDetails from './Pages/Staff-Member/TableReservationDetails';
 import CustomerDashBoard from './Pages/Customer/CustomerDashBoard';
 import CustomerUserProfile from './Pages/Customer/UserProfile';
@@ -34,71 +39,47 @@ import DeliveryOrderDetails from './Pages/Deliverer/OrderDetails';
 import SupplierDashBoard from './Pages/Supplier/SupplierDashBoard';
 import SupplierUserProfile from './Pages/Supplier/UserProfile';
 import Messages from './Pages/Supplier/Messages';
+import FrogotPassword from './Pages/FrogotPassword';
 import PasswordReset from './Pages/PasswordReset';
 import { Cards, Roles } from './Data/DashBoardLinks';
+import PlaceOrder from './Pages/Staff-Member/PlaceOrder';
+import AddTables from './Pages/SharedOnes/AddTable';
+import ViewTables from './Pages/SharedOnes/ViewTables';
+import ViewTable from './Pages/SharedOnes/ViewTable';
+import AddFoods from './Pages/SharedOnes/AddFoods';
+import ViewFood from './Pages/SharedOnes/ViewFood';
+import ViewOffers from './Pages/SharedOnes/ViewOffers';
+import ViewOffer from './Pages/SharedOnes/ViewOffer';
+import ViewFoods from './Pages/SharedOnes/ViewFoods';
 import "aos/dist/aos.css";
 import Aos from 'aos';
 import { MenuItems } from './Data/Menu';
 import { TableColumns, ViewThings } from './Data/Content';
-import AuthState from './Context/Auth/AuthState';
-// import RequireAuth from './components/Routing/RequireAuth';
-import AdminAddUser from './Pages/Admin/AddUser';
-import AdminViewUser from './Pages/Admin/AdminViewUser';
-import AdminAddTables from './Pages/Admin/AdminAddTable';
-import AdminViewTables from './Pages/Admin/AdminViewTables';
-import AdminViewTable from './Pages/Admin/AdminViewTable';
-import AdminAddFoods from './Pages/Admin/AddFoods';
-import AdminViewFoods from './Pages/Admin/AdimnViewFoods';
-import AdminViewFood from './Pages/Admin/AdminViewFood';
-import ManagerViewUser from './Pages/Manager/ManagerViewUser';
-import ManagerAddTables from './Pages/Manager/ManagerAddTable';
-import ManagerViewTables from './Pages/Manager/ManagerViewTables';
-import ManagerViewTable from './Pages/Manager/ManagerViewTable';
-import ManagerAddFoods from './Pages/Manager/AddFoods';
-import ManagerViewFoods from './Pages/Manager/ManagerViewFoods';
-import ManagerViewFood from './Pages/Manager/ManagerViewFood';
-import ManagerViewOffers from './Pages/Manager/ViewOffers';
-import ManagerViewOffer from './Pages/Manager/ViewOffer';
-import StaffMemberAddOffers from './Pages/Staff-Member/AddOffers';
-import StaffMemberViewOffers from './Pages/Staff-Member/ViewOffers';
-import StaffMemberViewOffer from './Pages/Staff-Member/ViewOffer';
-import StaffMemberViewFoods from './Pages/Staff-Member/StaffMemberViewFoods';
-import StaffMemberViewTables from './Pages/Staff-Member/StaffMemberViewTables';
-import StaffMemberOrderDetails from './Pages/Staff-Member/OrderDetails';
-import StaffMemberTableReservationDetails from './Pages/Staff-Member/TableReservationDetails';
-import StaffMemberPlaceOrder from './Pages/Staff-Member/PlaceOrder';
-import FrogotPassword from './Pages/ForgotPassword';
 
 function App() {
   useEffect(() => {
     Aos.init({duration:1000});
   }, [])
   const scrollToTop = ()=>{
-    if(window.pageYOffset > 200)
-    {
-      window.scrollTo(
-        {
-          top: 0, 
-          behavior: "smooth",
-        }
-      );
+    if(window.pageYOffset > 200){
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
     }
-  }
+}
   return (
-    <div>
-      <AuthState>
-          <Fragment>
-          <div className="App">
-            <ScrollToTop/> 
-            <Navbar ScrollToTop={scrollToTop}/>
-            <Routes>
-                <Route path="/" element={<Home ScrollToTop={scrollToTop}/>}/>
-                <Route path="/Menu" element={<Menu MenuItems = {MenuItems}/>}/>
-                <Route path="/login" element={<LoginPg/>}/>
-                <Route path="/FrogotPassword" element={<FrogotPassword/>}/>
-                <Route path="/ResetPassword/:token" element={<PasswordReset/>}/>
+    <div className="App">
+      <ScrollToTop/> 
+      <Navbar ScrollToTop={scrollToTop}/>
+      <Routes>
+          <Route path="/" element={<Home ScrollToTop={scrollToTop}/>}/>
+          <Route path="/Menu" element={<Menu MenuItems = {MenuItems}/>}/>
+          <Route path="/login" element={<LoginPg/>}/>
+          <Route path="/FrogotPassword" element={<FrogotPassword/>}/>
+          <Route path="/ResetPassword/:token" element={<PasswordReset/>}/>
 
- 
+
         {/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Admin ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  */}
           
           <Route path="/Admin/DashBoard" element={<AdminDashBoard Navs = {Roles[0]} Card = {Cards[0]}/>} />
@@ -112,49 +93,68 @@ function App() {
           <Route path="/Admin/View-Foods" element={<ViewFoods Tables={TableColumns[0]}/>}/>
           <Route path="/Admin/View-Food" element={<ViewFood View = {ViewThings[0]}/>}/>
 
-              {/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Staff-Member ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  */}
-                
-                <Route path="/Staff-Member/DashBoard" element={<StaffMemberDashBoard Navs = {Roles[1]}/>}/>
-                <Route path="/Staff-Member/Add-Offers" element={<StaffMemberAddOffers/>}/>
-                <Route path="/Staff-Member/View-Offers" element={<StaffMemberViewOffers Tables={TableColumns[5]}/>}/>
-                <Route path="/Staff-Member/View-Offer" element={<StaffMemberViewOffer View = {ViewThings[1]}/>}/>
-                <Route path="/Staff-Member/View-Foods" element={<StaffMemberViewFoods Tables={TableColumns[0]}/>}/>
-                <Route path="/Staff-Member/View-Tables" element={<StaffMemberViewTables Tables={TableColumns[1]}/>}/>
-                <Route path="/Staff-Member/Order-Details" element={<StaffMemberOrderDetails/>}/>
-                <Route path="/Staff-Member/Table-Reservation-Details" element={<StaffMemberTableReservationDetails/>}/>
-                <Route path="/Staff-Member/My-Profile" element={<StaffMemberUserProfile/>}/>
-                <Route path="/Staff-Member/Place-Order" element={<StaffMemberPlaceOrder/>}/>
+        {/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Manager ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  */}
+          
+          <Route path="/Manager/DashBoard" element={<ManagerDashBoard/>}/>
+          <Route path="/Manager/Add-User" element={<AddOutletStaff/>}/>
+          <Route path="/Manager/View-User" element={<ViewStaffMember/>}/>
+          <Route path="/Manager/My-Profile" element={<ManagerUserProfile/>}/>
+          <Route path="/Manager/Report-Generation" element={<ReportGeneration/>}/>
+          <Route path="/Manager/ReportPreview" element={<ReportPreview/>}/>
+          <Route path="/Manager/AddStcok" element={<AddStockItem/>}/>
+          <Route path="/Manager/ViewStock" element={<ViewStock Tables={TableColumns[2]}/>}/>
+          <Route path="/Manager/ViewItem" element={<ViewItem/>}/>
+          <Route path="/Manager/Add-Table" element={<AddTables/>}/>
+          <Route path="/Manager/View-Tables" element={<ViewTables Tables={TableColumns[1]}/>}/>
+          <Route path="/Manager/View-Table" element={<ViewTable View = {ViewThings[2]}/>}/>
+          <Route path="/Manager/Add-Foods" element={<AddFoods/>}/>
+          <Route path="/Manager/Add-Offers" element={<AddOffers/>}/>
+          <Route path="/Manager/View-Foods" element={<ViewFoods Tables={TableColumns[0]}/>}/>
+          <Route path="/Manager/View-Food" element={<ViewFood View = {ViewThings[0]}/>}/>
+          <Route path="/Manager/View-Offers" element={<ViewOffers Tables={TableColumns[5]}/>}/>
+          <Route path="/Manager/View-Offer" element={<ViewOffer View = {ViewThings[1]}/>}/>
 
-                
-              {/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Customer ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  */}
-                  
-                <Route path="/Customer/DashBoard" element={<CustomerDashBoard/>}/>
-                <Route path="/Customer/My-Profile" element={<CustomerUserProfile/>}/>     
-                <Route path="/Customer/Place-Order" element={<Order/>}/>     
-                <Route path="/Customer/My-Cart" element={<Cart/>}/>     
-                <Route path="/Customer/My-Orders" element={<MyOrders/>}/>   
-                <Route path="/Customer/Table-Reservation" element={<TableReservationDetails/>}/>     
-                <Route path="/Customer/Add-Review" element={<AddReview/>}/>     
-                <Route path="/Customer/Delivery-Tracking" element={<DeliveryTracking/>}/>     
+        {/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Staff-Member ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  */}
+          
+          <Route path="/Staff-Member/DashBoard" element={<StaffMemberDashBoard/>}/>
+          <Route path="/Staff-Member/Add-Offers" element={<AddOffers/>}/>
+          <Route path="/Staff-Member/View-Offers" element={<ViewOffers Tables={TableColumns[5]}/>}/>
+          <Route path="/Staff-Member/View-Offer" element={<ViewOffer View = {ViewThings[1]}/>}/>
+          <Route path="/Staff-Member/View-Foods" element={<ViewFoods Tables={TableColumns[0]}/>}/>
+          <Route path="/Staff-Member/View-Tables" element={<ViewTables Tables={TableColumns[1]}/>}/>
+          <Route path="/Staff-Member/Order-Details" element={<OrderDetails/>}/>
+          <Route path="/Staff-Member/Table-Reservation-Details" element={<TableReservationDetails/>}/>
+          <Route path="/Staff-Member/My-Profile" element={<StaffMemberUserProfile/>}/>
+          <Route path="/Staff-Member/Place-Order" element={<PlaceOrder/>}/>
 
-              {/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Deliverer ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  */}
-                  
-                <Route path="/Deliverer/DashBoard" element={<DelivererDashBoard/>}/>
-                <Route path="/Deliverer/My-Profile" element={<DelivererUserProfile/>}/>     
-                <Route path="/Deliverer/Place-Order" element={<DeliveryOrderDetails/>}/>     
-              
-              {/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Supplier ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  */}
-                  
-                <Route path="/Supplier/DashBoard" element={<SupplierDashBoard/>}/>
-                <Route path="/Supplier/My-Profile" element={<SupplierUserProfile/>}/>     
-                <Route path="/Supplier/Messages" element={<Messages/>}/> 
-                
-            </Routes>
-            <Footer/>
-          </div>
-          </Fragment>
-      </AuthState>
+          
+        {/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Customer ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  */}
+            
+          <Route path="/Customer/DashBoard" element={<CustomerDashBoard/>}/>
+          <Route path="/Customer/My-Profile" element={<CustomerUserProfile/>}/>     
+          <Route path="/Customer/Place-Order" element={<Order/>}/>     
+          <Route path="/Customer/My-Cart" element={<Cart/>}/>     
+          <Route path="/Customer/My-Orders" element={<MyOrders/>}/>   
+          <Route path="/Customer/Table-Reservation" element={<TableReservationDetails/>}/>     
+          <Route path="/Customer/Add-Review" element={<AddReview/>}/>     
+          <Route path="/Customer/Delivery-Tracking" element={<DeliveryTracking/>}/>     
+
+        {/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Deliverer ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  */}
+            
+          <Route path="/Deliverer/DashBoard" element={<DelivererDashBoard/>}/>
+          <Route path="/Deliverer/My-Profile" element={<DelivererUserProfile/>}/>     
+          <Route path="/Deliverer/Place-Order" element={<DeliveryOrderDetails/>}/>     
+        
+        {/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Supplier ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  */}
+            
+          <Route path="/Supplier/DashBoard" element={<SupplierDashBoard/>}/>
+          <Route path="/Supplier/My-Profile" element={<SupplierUserProfile/>}/>     
+          <Route path="/Supplier/Messages" element={<Messages/>}/> 
+
+      </Routes>
+      <Footer/>
     </div>
+
   );
 }
 
