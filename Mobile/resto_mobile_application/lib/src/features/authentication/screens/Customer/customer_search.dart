83e3e9fe2750ed85c1f55881bbf3e9834c8ecd87
PraveenChamod <praveenchamod23@gmail.com>
2023-02-22 02:48:54 +0530

    Modified Customer Home Screen

--- Mobile/resto_mobile_application/lib/src/features/authentication/screens/Customer/customer_search.dart
+++ Mobile/resto_mobile_application/lib/src/features/authentication/screens/Customer/customer_search.dart
@@ -1,9 +1,100 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../../../../common_widgets/background_image.dart';
+import '../../../../constants/image_strings.dart';
+import '../../models/food_model.dart';
+import '../login_screen.dart';
 
-class CustomerSearch extends StatelessWidget {
+class CustomerSearch extends StatefulWidget {
   const CustomerSearch({Key? key}) : super(key: key);
+
+  @override
+  State<CustomerSearch> createState() => _CustomerSearchState();
+}
+
+class _CustomerSearchState extends State<CustomerSearch> {
+
+  List<Map<String, String>> foodItems = [
+    {
+      "foodImagePath": "assets/Food Types/Pizza/Cheese_Pizza.jpg",
+      "foodName": "Pizza",
+      "foodPrice": "4.70",
+      "foodSpecialIngredient": "With Almond Milk",
+    },
+    {
+      "foodImagePath": "assets/Food Types/Burger/Chicken_Burger.jpg",
+      "foodName": "Burger",
+      "foodPrice": "4.50",
+      "foodSpecialIngredient": "With Coconut Milk",
+    },
+    {
+      "foodImagePath": "assets/Food Types/Koththu/Chicken_Koththu.jpg",
+      "foodName": "Koththu",
+      "foodPrice": "5.60",
+      "foodSpecialIngredient": "With Chocolate",
+    },
+    {
+      "foodImagePath": "assets/Food Types/Rice/Veg_Rice.jpg",
+      "foodName": "Rice",
+      "foodPrice": "3.60",
+      "foodSpecialIngredient": "With Chilies",
+    },
+    {
+      "foodImagePath": "assets/Food Types/Pizza/Cheese_Pizza.jpg",
+      "foodName": "Pizza",
+      "foodPrice": "4.70",
+      "foodSpecialIngredient": "With Almond Milk",
+    },
+    {
+      "foodImagePath": "assets/Food Types/Koththu/Chicken_Koththu.jpg",
+      "foodName": "Koththu",
+      "foodPrice": "5.60",
+      "foodSpecialIngredient": "With Chocolate",
+    },
+  ];
+
+  static List<FoodModel> main_food_list = [
+    FoodModel(
+      "assets/Food Types/Pizza/Cheese_Pizza.jpg",
+      "Pizza",
+      "4.60",
+      'With Almond Milk',
+    ),
+    FoodModel(
+      "assets/Food Types/Burger/Chicken_Burger.jpg",
+      "Burger",
+      "4.50",
+      'With Coconut Milk',
+    ),
+    FoodModel(
+      "assets/Food Types/Koththu/Chicken_Koththu.jpg",
+      "Koththu",
+      "5.60",
+      'With Chocolate',
+    ),
+    FoodModel(
+      "assets/Food Types/Rice/Veg_Rice.jpg",
+      "Rice",
+      "3.60",
+      'With Chilies',
+    ),
+    FoodModel(
+      "assets/Food Types/Koththu/Chicken_Koththu.jpg",
+      "Koththu",
+      "5.60",
+      'With Chocolate',
+    ),
+    FoodModel(
+      "assets/Food Types/Pizza/Cheese_Pizza.jpg",
+      "Pizza",
+      "4.60",
+      'With Almond Milk',
+    ),
+  ];
+  void updateList(String value){
+    //This is the function that will filter our list
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,9 +109,6 @@ class CustomerSearch extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Container(
-                      height: MediaQuery.of(context).size.height/8,
-                      //height: 60,
-                      width: MediaQuery.of(context).size.width,
                       margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
                       child: Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 25.0),
@@ -52,55 +140,22 @@ class CustomerSearch extends StatelessWidget {
                         ),
                       ),
                     ),
-                    //const SizedBox(height: 10,),
                     Container(
                       height: 7 * MediaQuery.of(context).size.height/8,
-                      //height: MediaQuery.of(context).size.height - 80,
                       width: MediaQuery.of(context).size.width,
-                      //width: 280,
-                      child: ListView(
+                      child: ListView.builder(
                         scrollDirection: Axis.vertical,
-                        children: const <Widget>[
-                          FoodTile(
-                            foodImagePath: "assets/Food Types/Pizza/Cheese_Pizza.jpg",
-                            foodName: "Pizza",
-                            foodPrice: "4.60",
-                            foodSpecialIngredient: 'With Almond Milk',
-                          ),
-                          FoodTile(
-                            foodImagePath: "assets/Food Types/Burger/Chicken_Burger.jpg",
-                            foodName: "Burger",
-                            foodPrice: "4.50",
-                            foodSpecialIngredient: 'With Coconut Milk',
-                          ),
-                          FoodTile(
-                            foodImagePath: "assets/Food Types/Koththu/Chicken_Koththu.jpg",
-                            foodName: "Koththu",
-                            foodPrice: "5.60",
-                            foodSpecialIngredient: 'With Chocolate',
-                          ),
-                          FoodTile(
-                            foodImagePath: "assets/Food Types/Rice/Veg_Rice.jpg",
-                            foodName: "Rice",
-                            foodPrice: "3.60",
-                            foodSpecialIngredient: 'With Chilies',
-                          ),
-                          FoodTile(
-                            foodImagePath: "assets/Food Types/Koththu/Chicken_Koththu.jpg",
-                            foodName: "Koththu",
-                            foodPrice: "5.60",
-                            foodSpecialIngredient: 'With Chocolate',
-                          ),
-                          FoodTile(
-                            foodImagePath: "assets/Food Types/Rice/Veg_Rice.jpg",
-                            foodName: "Rice",
-                            foodPrice: "3.60",
-                            foodSpecialIngredient: 'With Chilies',
-                          ),
-                        ],
+                        itemCount: foodItems.length,
+                        itemBuilder: (context, index) {
+                          return SearchFoodTile(
+                            foodImagePath: foodItems[index]["foodImagePath"] ?? '',
+                            foodName: foodItems[index]["foodName"] ?? '',
+                            foodPrice: foodItems[index]["foodPrice"] ?? '',
+                            foodSpecialIngredient: foodItems[index]["foodSpecialIngredient"] ?? '',
+                          );
+                        },
                       ),
                     ),
-
                   ],
                 ),
               ),
@@ -111,18 +166,15 @@ class CustomerSearch extends StatelessWidget {
   }
 }
 
-
-
-
 //Food Tile stl
-class FoodTile extends StatelessWidget {
+class SearchFoodTile extends StatelessWidget {
 
   final String foodImagePath;
   final String foodName;
   final String foodSpecialIngredient;
   final String foodPrice;
 
-  const FoodTile({Key? key,
+  const SearchFoodTile({Key? key,
     required this.foodImagePath,
     required this.foodName,
     required this.foodPrice,
@@ -131,82 +183,74 @@ class FoodTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 25.0),
-      child: Container(
-        padding: const EdgeInsets.all(12),
-        width: 200,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          color: Colors.black54,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            //Food Image
-            Center(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-                child: Image.asset(
-                  foodImagePath,
-                  width: 125,
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (_){
+              return const LoginScreen();
+            },
+          ),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: Colors.black54,
+          ),
+          child: Row(
+            children: [
+              Container(
+                //width: MediaQuery.of(context).size.width/4,
+                child: Center(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Image.asset(
+                      foodImagePath,
+                      width: 70,
+                    ),
+                  ),
                 ),
               ),
-            ),
-
-            //Food Name & Special Ingredient
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    foodName,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
+              const SizedBox(width: 15,),
+              //Food Name & Special Ingredient
+              Container(
+                //width: MediaQuery.of(context).size.width/3,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      foodName,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 5,),
-                  Text(
-                    foodSpecialIngredient,
-                    style: const TextStyle(
-                      color: Colors.white70,
-                      fontSize: 15.0,
+                    const SizedBox(height: 5,),
+                    Text(
+                      foodSpecialIngredient,
+                      style: const TextStyle(
+                        color: Colors.white70,
+                        fontSize: 15.0,
+                      ),
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 5,),
+                    Text(
+                      "\$$foodPrice",
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-
-            //Price
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "\$$foodPrice",
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 15.0,
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(4),
-                    decoration: BoxDecoration(
-                      color: const Color(0xFFfebf10),
-                      borderRadius: BorderRadius.circular(6.0),
-                    ),
-                    child: const Icon(
-                      Icons.add,
-                      color: Colors.black,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
