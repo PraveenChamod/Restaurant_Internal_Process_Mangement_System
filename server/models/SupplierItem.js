3c6da6b4d61c3999710089440424b3ab1b3ff4bd
Dilmith98 <dilmithdilhara.gg@gmail.com>
2023-02-28 21:51:14 +0530

    create ordering controll

---
+++ server/models/SupplierItem.js
@@ -0,0 +1,31 @@
+import mongoose  from "mongoose";
+
+const SupplierItemSchema = mongoose.Schema({
+    Items:{
+        type:String,
+        required:true
+    },
+
+    Quantity:{
+        type:Number,
+        required:true
+    },
+
+    Date:{
+        type:Date,
+        required:true,
+        default:Date.now()
+    },
+
+},
+{
+    toJSON: { 
+        virtuals: true,
+    },
+    toObject: { virtuals: true },
+    timestamps: true
+});
+
+const SupplierItem = mongoose.model('SupplierItem',SupplierItemSchema);
+
+export default SupplierItem;
