1ca3465ba35eb5f96b5629162ecd5976e408a350
imAravinda <acfernando1999@gmail.com>
2023-03-21 00:34:08 +0530

    resolve merge conflicts

--- client/src/Pages/Staff-Member/StaffMemberViewFoods.js
+++ client/src/Pages/Staff-Member/StaffMemberViewFoods.js
@@ -1,9 +1,15 @@
 import FoodDetails from "../../components/shared/FoodDetails/FoodDetails";
+import Spinner from "../../components/shared/Spinner/Spinner";
+import useFetch from "../../Hooks/useFetch";
 
 const StaffMemberViewFoods = (props) => {
+    const {data,isPending}= useFetch('api/v1/Foods');
+    console.log(data?.data?.foods);
+    const foods = data?.data?.foods;
     return (  
         <>
-            <FoodDetails BackRoutes={props.BackRoutes}/>
+        {isPending && <Spinner/>}
+        {data && <FoodDetails data1={foods} BackRoutes={props.BackRoutes}/>}
         </>
     );
 }
