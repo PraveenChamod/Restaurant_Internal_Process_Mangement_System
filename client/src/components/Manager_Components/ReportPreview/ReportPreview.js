fcea739512ab0b0a285aae92372f48fa58d477a5
Nihara Bamunusinghe <akasuninihara@gmail.com>
2023-03-26 04:53:39 +0530

    pdf generation v0.2

--- client/src/components/Manager_Components/ReportPreview/ReportPreview.js
+++ client/src/components/Manager_Components/ReportPreview/ReportPreview.js
@@ -1,10 +1,9 @@
 import { Container, Header } from "../../shared/SharedElements/SharedElements";
 import * as l from './ReportPreviewElements';
 import { RegularButton } from "../../shared/SharedElements/Buttons";
-import { Link, useLocation, useParams } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useState } from "react";
 import PDFreport from "./PDFreport";
 import React from 'react';
 import logo from '../../../Images/restoLogodark.png'
@@ -19,6 +18,54 @@ const ReportPreview = (props) => {
 
     const totalPrices = orders.map(order => order.TotalPrice);
     const sumTotalPrices = totalPrices.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+    const foodQuantities = {};
+    let cashOnDeliveryCount = 0;
+    let onlinePaymentCount = 0;
+    orders.forEach(order => {
+        order.food.forEach(item => {
+            if (item.FoodName in foodQuantities) {
+            foodQuantities[item.FoodName].quantity += item.quantity;
+            foodQuantities[item.FoodName].totalPrice += item.price * item.quantity;
+            } else {
+            foodQuantities[item.FoodName] = {
+                quantity: item.quantity,
+                food: item.FoodName,
+                unitPrice: item.price,
+                totalPrice: item.price * item.quantity
+            };
+            }
+            if (item.PaymentMethod === "Cash On Delivery") {
+                cashOnDeliveryCount++;
+            } else {
+                onlinePaymentCount++;
+            }
+        });
+    });
+   
+    console.log(foodQuantities);
+
+    const customerFoodQuantities = {};
+    
+    orders.forEach(order => {
+        const customerName = order.customerName;
+        if (!customerFoodQuantities[customerName]) {
+            customerFoodQuantities[customerName] = 0;
+        }
+        customerFoodQuantities[customerName] += order.food.length;
+        
+    });
+
+    let maxCustomer = null;
+    let maxQuantity = -1;
+    for (const customer in customerFoodQuantities) {
+        if (customerFoodQuantities[customer] > maxQuantity) {
+            maxCustomer = customer;
+            maxQuantity = customerFoodQuantities[customer];
+          }
+    }
+        
+    const numOrders = orders.length;
 
     const handleClick = () => {
         PDFreport(orders, report);
@@ -40,35 +87,37 @@ const ReportPreview = (props) => {
                             </l.Th>
                         </l.Tr>
                         <l.Tr>
-                            <l.Th>Customer</l.Th>
+                            
                             <l.Th>Food Name</l.Th>
+                            <l.Th>Unit Price</l.Th>
                             <l.Th>Qty</l.Th>
-                            <l.Th>Price</l.Th>
-                            <l.Th>Payment Method</l.Th>
+                            <l.Th>Total Price</l.Th>
                         </l.Tr>
                         
                     </thead>
                     <tbody>
-                    {orders.map(order => (
-                            <>
-                                {order.food.map(item => (
-                                    <l.Tr key={item._id}>
-                                        <l.Td>{order.customerName}</l.Td>
-                                        <l.Td>{item.FoodName}</l.Td>
-                                        <l.Td>{item.quantity}</l.Td>
-                                        <l.Td>{order.TotalPrice}</l.Td>
-                                        <l.Td>{item.PaymentMethod}</l.Td>
-                                    </l.Tr>
-                                ))}
-                            </>
+                        {Object.values(foodQuantities).map(item => (
+                            <l.Tr key={item.food}>
+                            <l.Td>{item.food}</l.Td>
+                            <l.Td>{item.unitPrice}</l.Td>
+                            <l.Td>{item.quantity}</l.Td>
+                            <l.Td>{item.totalPrice}</l.Td>
+                            </l.Tr>
                         ))}
+                          
                         <l.Tr>
-                            <l.Th></l.Th>
-                            <l.Th></l.Th>
-                            <l.Th>Income: </l.Th>
-                            <l.Th>{sumTotalPrices}</l.Th>
-                            <l.Th></l.Th>
-                        </l.Tr>  
+                            <l.Th colSpan="5">Income :  {sumTotalPrices}</l.Th>
+                        </l.Tr> 
+                        <l.Tr>
+                            <l.Th colSpan="5">Best customer :  {maxCustomer}</l.Th>
+                        </l.Tr> 
+                        <l.Tr>
+                            <l.Th colSpan="5">No of orders :  {numOrders}</l.Th>
+                        </l.Tr> 
+                        <l.Tr>
+                            <l.Th colSpan="2">Cash On Delivery :  {cashOnDeliveryCount}</l.Th>
+                            <l.Th colSpan="2">Card Payment :  {onlinePaymentCount}</l.Th>
+                        </l.Tr>
                         <l.Tr>
                             <l.Th colSpan="5">Report generated at {new Date().toLocaleString()}</l.Th>
                         </l.Tr>                    
