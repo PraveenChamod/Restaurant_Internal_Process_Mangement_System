16bf17ffa3f9d89c96dcef663ea50142395e0a72
Nihara Bamunusinghe <akasuninihara@gmail.com>
2023-03-26 22:46:17 +0530

    Manager view details of a table is complete

--- client/src/Pages/Manager/ManagerViewTable.js
+++ client/src/Pages/Manager/ManagerViewTable.js
@@ -1,9 +1,18 @@
+import { useParams } from "react-router-dom";
+import Spinner from "../../components/shared/Spinner/Spinner";
 import TableDetail from "../../components/shared/TableDetail/TableDetail";
+import useFetch from "../../Hooks/useFetch";
 
-const ManagerViewTableDetails = () => {
+const ManagerViewTableDetails = (props) => {
+    const {id} = useParams();
+    console.log(id);
+    const {data,isPending} = useFetch(`/api/v1/table/${id}`);
+    console.log(data);
+    const table = data?.data?.table
     return ( 
         <>
-            <TableDetail/>
+            {isPending && <Spinner/>}
+            {table && <TableDetail table={table}/>}
         </>
      );
 }
--- client/src/components/shared/TableDetail/TableDetail.js
+++ client/src/components/shared/TableDetail/TableDetail.js
@@ -1,16 +1,41 @@
+import { useState } from "react";
 import { FormButton, RegularButton } from "../SharedElements/Buttons";
 import { Container, Header } from "../SharedElements/SharedElements";
 import * as l from "./TableDetailElement";
-const TableDetail = () => {
+const TableDetail = (props) => {
+    const[TableNo,setTableNo] = useState(props.table.TableNo);
+    const[NoOfPersons,setNoOfPersons] = useState(props.table.NoOfPersons);
+    const[price,setprice] = useState(props.table.price);
+    const[Status,setStatus] = useState(props.table.Status);
     return ( 
         <Container>
         <Header>Table Details</Header>
         <l.Div>
             <l.Div1>
-                <l.TextFeild type="text" placeholder="Table No"/>
-                <l.TextFeild type="text" placeholder="Maximum No of Persons"/>
-                <l.TextFeild type="text" placeholder="Reservation Fee"/>
-                <l.TextFeild type="text" placeholder="Status(Available/Not)"/>
+                <l.TextFeild 
+                type="text" 
+                placeholder="Table No"
+                value={TableNo}
+                onChange={e=>setTableNo(e.target.value)}
+                />
+                <l.TextFeild 
+                type="text" 
+                placeholder="Maximum No of Persons"
+                value={NoOfPersons}
+                onChange={e=>setNoOfPersons(e.target.value)}
+                />
+                <l.TextFeild 
+                type="text" 
+                placeholder="Reservation Fee"
+                value={price}
+                onChange={e=>setprice(e.target.value)}
+                />
+                <l.TextFeild 
+                type="text" 
+                placeholder="Status(Available/Not)"
+                value={Status}
+                onChange={e=>setStatus(e.target.value)}
+                />
             </l.Div1>
             <l.Div2>
                 <l.Sec>
