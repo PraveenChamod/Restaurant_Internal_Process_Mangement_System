860b5cd044002d521d9ea8b247555fbb19c5dea5
imAravinda <acfernando1999@gmail.com>
2023-04-10 17:56:40 +0530

    resolve packages

--- server/routes/StocksOrderRoutes.js
+++ server/routes/StocksOrderRoutes.js
@@ -6,6 +6,6 @@ const StocksOrderRoutes = express.Router();
 StocksOrderRoutes.route('/SupplierOrder').post(addSupplierOrder);
 StocksOrderRoutes.route('/SupplierOrder').get(ViewSupplierOrder);
 StocksOrderRoutes.route('/stockorderbyid/:id').get(ViewSupplierOrderById);
-StocksOrderRoutes.route('/stockorderconfirmation').post(ConfirmStockOrder);
+StocksOrderRoutes.route('/stockorderconfirmation/:id').post(ConfirmStockOrder);
 
 export default StocksOrderRoutes;
