7f606fcc16c92fc74c3dbaaff42e707378409ad9
PraveenChamod <praveenchamod23@gmail.com>
2023-05-09 20:27:26 +0530

    Added getPackageItems Controller to the package controller.

--- server/routes/PackageRoutes.js
+++ server/routes/PackageRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addPackage, getPackages, removeItemsFromPackage, updatePackage } from "../controllers/PackageController.js";
+import { addPackage, getPackages, removeItemsFromPackage, updatePackage, getPackageItems } from "../controllers/PackageController.js";
 
 const PackageRoutes = express.Router();
 
@@ -7,5 +7,6 @@ PackageRoutes.route("/packages").post(addPackage);
 PackageRoutes.route("/packages").get(getPackages);
 PackageRoutes.route("/packages").patch(updatePackage);
 PackageRoutes.route("/packageitem").patch(removeItemsFromPackage);
+PackageRoutes.route("/packageitems/:id").get(getPackageItems);
 
 export default PackageRoutes;
