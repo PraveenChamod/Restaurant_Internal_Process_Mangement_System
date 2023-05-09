700fcc88eb6fcc8986599d3018ac3de14b2561fb
imAravinda <acfernando1999@gmail.com>
2023-05-09 14:09:23 +0530

    change some style

--- client/src/components/shared/TableDetails/TableDetails.js
+++ client/src/components/shared/TableDetails/TableDetails.js
@@ -12,36 +12,40 @@ const TableDetails = (props) => {
       <Header>Details Of All Tables</Header>
       <l.SubContainer>
         <l.Table>
-          <l.Tr>
-            <l.Th>Table No</l.Th>
-            <l.Th>Maximum No of Persons</l.Th>
-            <l.Th>Reservation Fee</l.Th>
-            <l.Th>Status</l.Th>
-            <l.Th></l.Th>
-          </l.Tr>
-          {tables.map((table) => {
-            return (
-              <l.Tr key={table._id}>
-                <l.Td>{table.TableNo}</l.Td>
-                <l.Td>{table.NoOfPersons}</l.Td>
-                <l.Td>{table.price}</l.Td>
-                <l.Td>{table.Status}</l.Td>
-                {user.Role === "Manager" || user.Role === "Admin" ? (
-                  <Link
-                    to={
-                      `/AdminView-Table/${table.id}` ||
-                      `/ManagerView-Table/${table.id}`
-                    }
-                    className="btn"
-                  >
-                    <l.Icon>
-                      <AiFillEye />
-                    </l.Icon>
-                  </Link>
-                ) : null}
-              </l.Tr>
-            );
-          })}
+          <l.Tr1>
+            <l.Tr>
+              <l.Th>Table No</l.Th>
+              <l.Th>Maximum No of Persons</l.Th>
+              <l.Th>Reservation Fee</l.Th>
+              <l.Th>Status</l.Th>
+              <l.Th></l.Th>
+            </l.Tr>
+            <l.data>
+              {tables.map((table) => {
+                return (
+                  <l.Tr key={table._id}>
+                    <l.Td>{table.TableNo}</l.Td>
+                    <l.Td>{table.NoOfPersons}</l.Td>
+                    <l.Td>{table.price}</l.Td>
+                    <l.Td>{table.Status}</l.Td>
+                    {user.Role === "Manager" || user.Role === "Admin" ? (
+                      <Link
+                        to={
+                          `/AdminView-Table/${table.id}` ||
+                          `/ManagerView-Table/${table.id}`
+                        }
+                        className="btn"
+                      >
+                        <l.Icon>
+                          <AiFillEye />
+                        </l.Icon>
+                      </Link>
+                    ) : null}
+                  </l.Tr>
+                );
+              })}
+            </l.data>
+          </l.Tr1>
         </l.Table>
       </l.SubContainer>
       <l.ButtonSection>
