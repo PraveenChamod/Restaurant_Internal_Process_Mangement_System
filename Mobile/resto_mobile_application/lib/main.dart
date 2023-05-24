c80d38f40dfd637f7bf1671c8979ca072f5607b3
PraveenChamod <praveenchamod23@gmail.com>
2023-05-24 14:14:13 +0530

    Remove the commented lines.

--- Mobile/resto_mobile_application/lib/main.dart
+++ Mobile/resto_mobile_application/lib/main.dart
@@ -6,35 +6,11 @@ import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:resto_mobile_application/src/constants/image_strings.dart';
 import 'package:resto_mobile_application/src/features/authentication/screens/splash_screen.dart';
 
-// void main() => runApp(
-//   DevicePreview(
-//     enabled: !kReleaseMode,
-//     builder: (context) => const MyApp(), // Wrap your app
-//   ),
-// );
-
-// void main() async {
-//   //Payment Intent
-//   WidgetsFlutterBinding.ensureInitialized();
-//   Stripe.publishableKey = publishableKey;
-//   runApp(
-//     DevicePreview(
-//       enabled: !kReleaseMode,
-//       builder: (context) => const MyApp(), // Wrap your app
-//     ),
-//   );
-// }
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Stripe.publishableKey = publishableKey;
   runApp(const MyApp());
 }
-
-
-// void main() {
-//   runApp(const MyApp());
-// }
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
